64ec3a9f6be17f269819808f38d33e12031d1f74
lovekaur2006 <kaurkamal98700@gmail.com>
2024-03-05 21:57:24 +0530

    Datatypes

---
+++ WEEK-1/dataTypes.js
@@ -0,0 +1,32 @@
+// JavaScript has 8 data types:
+// 1. String
+let name="Lovepreet";
+let lastName="Kaur";
+
+// 2. Number ------> JS numbers are always one type: double(64-bit floating number)
+let length=45;
+let breadth=6.8;
+let x=123e5; // 12300000
+let y=123e-5; // 0.00123
+
+// 3. BigInt ----> For too big integer values
+let i= BigInt("123456789012345678901234567890")
+
+// 4. Boolean
+let a= true;
+let b= false;
+
+// 5. Undefined ----------> The value and type of undefined is also undefined
+var car=undefined;
+
+// 6. Null
+let j=null;
+
+// 7. Symbol
+
+// 8. Object
+const person={firstName:"lovepreet", last_Name:"kaur"};
+
+
+
+// JavaScript has dynamic types. This means taht the same variable can be used to different data types
