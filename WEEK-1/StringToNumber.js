7a50cdeb663df5b96bc8e898d72dafcbcf15f3a1
lovekaur2006 <kaurkamal98700@gmail.com>
2024-03-07 14:07:47 +0530

    Operations

---
+++ WEEK-1/StringToNumber.js
@@ -0,0 +1,36 @@
+//-------------------Operations-----------------------
+let value = 7;
+let negValue = -value;
+console.log(negValue);
+
+console.log(5 + 8);
+console.log(8 - 5);
+console.log(5 * 8);
+console.log(5 / 8);
+console.log(5 % 8);
+console.log(5 ** 2);
+
+let str1 = "hello";
+let str2 = "World";
+let str3 = str1 + " " + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+console.log(num2);
+
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
+
+
